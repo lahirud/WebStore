d01f446db979995deed81a58a58ba565faef2094
Lahiru Dharmasena <lahiru.dharmasena@pearson.com>
2018-11-21 10:23:04 +0530

    EF added

---
+++ WebStore/Models/CakeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class CakeRepository : IRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CakeRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Cake> GetAllCakes()
+        {
+            return _appDbContext.Cakes;
+        }
+
+        public Cake GetCakeById(int id)
+        {
+            return _appDbContext.Cakes.FirstOrDefault(c => c.Id == id);
+
+        }
+    }
+}
