4221a1cce227e860e6f999f5622fcd5c5c7a9a34
Lilanga Gamage <lilanga.gamage@pearson.com>
2018-11-28 12:05:17 +0530

    initial UT project

--- WebStoreUnitTests/FeedbackControllerTest.cs
+++ WebStoreUnitTests/FeedbackControllerTest.cs
@@ -15,66 +15,26 @@ namespace WebStoreUnitTests
 
         public FeedbackControllerTest()
         {
-            feedbackRepository = new Mock<IFeedbackRepository>();
-            controller = new FeedbackController(feedbackRepository.Object);
         }
 
         [Fact]
         public void GetIndexShouldReturnOk()
         {
-            // act
-            var okResult = controller.Index();
-
-            // assert
-            Assert.IsType<ViewResult>(okResult);
         }
 
         [Fact]
         public void GetIndexShouldReturnFeedbacks()
         {
-            // arrange
-            List<Feedback> feedbacks = new List<Feedback>(){
-                new Feedback{FeedbackId=1, Email="test1@test.com", Name = "Test1", Message = "Test Message 1"},
-                new Feedback{FeedbackId=2, Email="test2@test.com", Name = "Test2", Message = "Test Message 2"},
-                new Feedback{FeedbackId=3, Email="test3@test.com", Name = "Test3", Message = "Test Message 3"},
-            };
-
-            feedbackRepository.Setup(repo => repo.GetAll()).Returns(feedbacks);
-
-            // act
-            var okResult = controller.Index() as ViewResult;
-            var viewModel = okResult.ViewData.Model as FeedbackViewModel;
-
-            // assert
-            var feedbackList = (viewModel.Feedbacks) as List<Feedback>;
-            Assert.Equal(3, feedbackList.Count);
         }
         
         [Fact]
         public void AddFeedbackShouldReturnOk()
         {
-            // arrange
-            var feedback = new Feedback{FeedbackId=1, Email="test1@test.com", Name = "Test1", Message = "Test Message 1"};
-
-            // act
-            var okResult = controller.Index(feedback);
-
-            // assert
-            Assert.IsType<RedirectToActionResult>(okResult);
         }
         
         [Fact]
         public void AddFeedbackShouldValidateEmail()
         {
-            // arrange
-            var feedback = new Feedback{FeedbackId=1, Email="test1", Name = "Test1", Message = "Test Message 1"};
-
-            // act
-            controller.ModelState.AddModelError("Email", "The email address is not entered in a correct format");
-            var okResult = controller.Index(feedback) as ViewResult;
-
-            // assert
-            Assert.Equal(1, okResult.ViewData.ModelState.ErrorCount);
         }
     }
 }
--- WebStoreUnitTests/HomeControllerTest.cs
+++ WebStoreUnitTests/HomeControllerTest.cs
@@ -14,52 +14,26 @@ namespace WebStoreUnitTests
 
         public HomeControllerTest()
         {
-            // arrange
-            cakesRepository = new MockCakeRepository();
-            controller = new HomeController(cakesRepository);
         }
 
         [Fact]
         public void GetIndexShouldReturnOk()
         {
-            // act
-            var okResult = controller.Index();
-
-            // assert
-            Assert.IsType<ViewResult>(okResult);
         }
 
         [Fact]
         public void GetIndexShouldReturnList()
         {
-            // act
-            var okResult = controller.Index() as ViewResult;
-            var viewModel = okResult.ViewData.Model as HomeViewModel;
-            // assert
-            Assert.IsAssignableFrom<IEnumerable<Cake>>(viewModel.Cakes);
         }
 
         [Fact]
         public void GetIndexShouldReturnItemsCount()
         {
-            // act
-            var okResult = controller.Index() as ViewResult;
-            var viewModel = okResult.ViewData.Model as HomeViewModel;
-
-            // assert
-            var cakesList = (viewModel.Cakes) as List<Cake>;
-            Assert.Equal(4, cakesList.Count);
         }
 
         [Fact]
         public void GetDetailsShouldReturnValidItem()
         {
-            // act
-            var okResult = controller.Detail(2) as ViewResult;
-            var viewModel = okResult.ViewData.Model as Cake;
-
-            // assert
-            Assert.Equal("Blueberry Cheese Cake", viewModel.Name);
         }
     }
 }
